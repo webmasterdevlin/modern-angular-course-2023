69dc33aa9e8550b82b1f4e655457974e44b09f6b
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-18 13:18:10 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
