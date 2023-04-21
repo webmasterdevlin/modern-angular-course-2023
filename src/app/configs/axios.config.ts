32ca1eb1216f37f4737906c30cfb6d7a85857499
devlinduldulao <webmasterdevlin@gmail.com>
2023-04-21 22:54:10 +0200

    update

--- src/app/configs/axios.config.ts
+++ src/app/configs/axios.config.ts
@@ -4,9 +4,13 @@ export const api = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com/',
 });
 
+api.interceptors.request.use(config => {
+  // do something before request is sent
+  return config;
+});
+
 export const EndPoints = {
   todos: 'todos',
-  users: 'users',
   posts: 'posts',
 } as const;
 
