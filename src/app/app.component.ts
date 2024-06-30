5b6d2ef439f01a2eacc0ca460d6c3ef31f25ba67
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-06-30 20:30:20 +0200

    chore: Update app.component.ts and package.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,13 @@ import { FooterComponent } from './shared/components/footer.component';
   standalone: true,
   imports: [RouterModule, NavBarComponent, FooterComponent],
   template: `
-    <app-nav-bar></app-nav-bar>
-    <div class="container prose mx-auto">
-      <router-outlet></router-outlet>
-    </div>
-    <app-footer></app-footer>
+    <main class="container prose mx-auto">
+      <app-nav-bar></app-nav-bar>
+      <div class="container prose mx-auto">
+        <router-outlet></router-outlet>
+      </div>
+      <app-footer></app-footer>
+    </main>
   `,
 })
 export class AppComponent {}
